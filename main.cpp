47e0049259e49d32d228f734d9a0acb77bc32f56
IshabulHQ <IshabulHQ@users.noreply.github.com>
2019-05-13 09:32:48 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+int rollDice()
+{
+    int roll;
+    int min = 1; // the min number a die can roll is 1
+    int max = 6;// this->dieSize; // the max value is the die size
+
+    roll = rand() % (max - min + 1) + min;
+
+    return roll;
+}
+int turn(int player, int square) {
+    int roll, next;
+    while (true) {
+        roll = rollDice();
+        printf("Player %d, on square %d, rolls a %d", player, square, roll);
+        if (square + roll > 100) {
+            printf(" but cannot move.\n");
+        } else {
+            square += roll;
+            printf(" and moves to square %d\n", square);
+            if (square == 100)
+                return 100;
+
+            if (square < next) {
+                printf("Landed on a ladder. Climb up to %d.\n", next);
+                square = next;
+            } else if (next < square) {
+                printf("Oops! landed on a snake. Slither down to %d.\n", next);
+                square = next;
+            }
+        }
+
+    }
+}
+
+
+int main(){
+
+
+    return 0;
+}
